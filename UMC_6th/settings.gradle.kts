ef20a6911865feb634f90f772e8909548cbf95b4
fprtmjinho <fjrtmjinho@gmail.com>
2024-04-02 21:27:19 +0900

    [#2] Android 초기 파일 생성

---
+++ UMC_6th/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "UMC_6th"
+include(":app")
+ 
