68cc012c1b329e3c99fc664b81cea69bfe9f901e
hyowon0204 <ally0204@naver.com>
2024-04-09 17:40:53 +0900

    [kara/#3] 1주차

---
+++ UMC_6th/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myfirstapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        if (savedInstanceState == null) {
+            binding.mainBottomNavigation.selectedItemId = R.id.fragment_home
+        }
+    }
+}
