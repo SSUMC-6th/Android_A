b70694f53356236be40193ccf2d2cd9ac2682d9c
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-04-16 19:12:58 +0900

    [Nunu/#7] feat 2주차 미션

--- UMC_6th/app/src/main/java/com/example/umc_6th/MainActivity.kt
+++ UMC_6th/app/src/main/java/com/example/umc_6th/MainActivity.kt
@@ -1,11 +1,84 @@
 package com.example.umc_6th
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.umc_6th.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        resultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val albumTitle = result.data?.getStringExtra("albumTitle") ?: "No Title Provided"
+                Toast.makeText(this, albumTitle, Toast.LENGTH_LONG).show()
+            }
+        }
+
+        setBottomNavigationView()
+
+        if(savedInstanceState == null){
+            binding.mainBottomNavigation.selectedItemId = R.id.fragment_home
+        }
+
+        binding.layoutPlayContainer.setOnClickListener(){
+            val intent = Intent(this, SongActivity::class.java)
+            intent.putExtra("songTitle", binding.txPlayTitle.text)
+            intent.putExtra("songArtist", binding.txPlayArtist.text)
+            resultLauncher.launch(intent)
+        }
+
+
+    }
+
+
+    private fun setBottomNavigationView() {
+        binding.mainBottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_home -> {
+                    supportFragmentManager.beginTransaction().replace(
+                        R.id.main_container,
+                        HomeFragment()
+                    ).commit()
+                    true
+                }
+                R.id.fragment_look -> {
+                    supportFragmentManager.beginTransaction().replace(
+                        R.id.main_container,
+                        LookFragment()
+                    ).commit()
+                    true
+                }
+                R.id.fragment_search -> {
+                    supportFragmentManager.beginTransaction().replace(
+                        R.id.main_container,
+                        SearchFragment()
+                    ).commit()
+                    true
+                }
+                R.id.fragment_locker -> {
+                    supportFragmentManager.beginTransaction().replace(
+                        R.id.main_container,
+                        LockerFragment()
+                    ).commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
