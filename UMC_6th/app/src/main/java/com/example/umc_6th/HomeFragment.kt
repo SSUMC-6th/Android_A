66d18d000fa86d738555d0248867bfd86a07a0e4
leesumin0526 <leesumin0526@gmail.com>
2024-04-20 20:29:12 +0900

    [maro/#13] feat : ViewPager Background

--- UMC_6th/app/src/main/java/com/example/umc_6th/HomeFragment.kt
+++ UMC_6th/app/src/main/java/com/example/umc_6th/HomeFragment.kt
@@ -40,6 +40,12 @@ class HomeFragment : Fragment() {
         binding.homeBannerVp.adapter = bannerAdapter
         binding.homeBannerVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
+        val pannelAdpater = PannelVpAdapter(this)
+        pannelAdpater.addFragment(PannelFragment(R.drawable.img_first_album_default))
+        pannelAdpater.addFragment(PannelFragment(R.drawable.img_first_album_default))
+        binding.homePannelBackgroundVp.adapter = pannelAdpater
+        binding.homePannelBackgroundVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
         return binding.root
     }
 }
