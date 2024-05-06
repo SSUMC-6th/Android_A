b625be471be21a2bed1ddcc46871997d140657e3
leesumin0526 <leesumin0526@gmail.com>
2024-05-06 23:24:56 +0900

    [maro/#18] feat : Splash 화면 Theme으로 구현

--- UMC_6th/app/src/main/java/com/example/umc_6th/MainActivity.kt
+++ UMC_6th/app/src/main/java/com/example/umc_6th/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.SplashTheme)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
