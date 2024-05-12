f60d975b8a97cba93aaed09af47acdcbf2a9d529
BAEK0111 <baekjw111@gmail.com>
2024-05-12 18:59:29 +0900

    [simba/#23] feat::5주차 구현 완료 mediaplayer 구현 sharedpreferences 구현

--- UMC_6th/app/src/main/java/com/example/umc_6th/SongActivity.kt
+++ UMC_6th/app/src/main/java/com/example/umc_6th/SongActivity.kt
@@ -1,11 +1,13 @@
 package com.example.umc_6th
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.umc_6th.databinding.ActivitySongBinding
+import com.google.gson.Gson
 import java.util.Timer
 
 class SongActivity : AppCompatActivity() {
@@ -13,6 +15,9 @@ class SongActivity : AppCompatActivity() {
     lateinit var binding: ActivitySongBinding
     lateinit var song: Song
     lateinit var timer : Timer
+
+    private var mediaPlayer : MediaPlayer? = null
+    private var gson : Gson = Gson()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +52,27 @@ class SongActivity : AppCompatActivity() {
         }
     }
 
+    override fun onPause() {
+        super.onPause()
+        setPlayerStatus(false)
+
+        song.second = (song.playTime * binding.songProgressSb.progress) / 100000
+        val sharedPreferences = getSharedPreferences("song", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val songToJson = gson.toJson(song)
+        editor.putString("songData", songToJson)
+        Log.d("songData", songToJson.toString())
+        editor.apply()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.interrupt()
+        mediaPlayer?.release() // 미디어 플레이어가 갖고 있던 리소스를 해제한다.
+        mediaPlayer = null // 미디어 플레이어를 해제한다.
+    }
+
+
     private fun initSong(){
         if(intent.hasExtra("title") && intent.hasExtra("singer")){
             song = Song(
@@ -54,7 +80,8 @@ class SongActivity : AppCompatActivity() {
                 intent.getStringExtra("singer")!!,
                 intent.getIntExtra("second",0),
                 intent.getIntExtra("playTime",0),
-                intent.getBooleanExtra("isPlaying",false)
+                intent.getBooleanExtra("isPlaying",false),
+                intent.getStringExtra("music")!!
             )
         }
         startTimer()
@@ -67,6 +94,8 @@ class SongActivity : AppCompatActivity() {
         binding.songEndTimeTv.text = String.format("%02d:%02d",song.playTime / 60,song.playTime % 60)
         binding.songProgressSb.progress = (song.second * 1000 / song.playTime)
 
+        val music = resources.getIdentifier(song.music,"raw",this.packageName)
+        mediaPlayer = MediaPlayer.create(this,music)
         setPlayerStatus(song.isPlaying)
     }
 
@@ -74,12 +103,17 @@ class SongActivity : AppCompatActivity() {
         song.isPlaying = isPlaying
         timer.isPlaying = isPlaying
 
-        if(isPlaying){
+        if(isPlaying){ // 재생중
             binding.songMiniplayerIv.visibility= View.GONE
             binding.songPauseIv.visibility = View.VISIBLE
-        } else{
+            mediaPlayer?.start()
+        } else{ // 일시정지
             binding.songMiniplayerIv.visibility=View.VISIBLE
             binding.songPauseIv.visibility = View.GONE
+
+            if(mediaPlayer?.isPlaying == true){
+                mediaPlayer?.pause()
+            }
         }
     }
 
