e693a85472878c9b80a1b8204fc86ba51425e709
BAEK0111 <143854581+BAEK0111@users.noreply.github.com>
2024-05-14 19:52:13 +0900

    [simba/#23] feat::5주차 구현 완료 (#27)
    mediaplayer 구현
    sharedpreferences 구현

--- UMC_6th/app/src/main/java/com/example/umc_6th/MainActivity.kt
+++ UMC_6th/app/src/main/java/com/example/umc_6th/MainActivity.kt
@@ -8,27 +8,22 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.umc_6th.databinding.ActivityMainBinding
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
     lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private var song : Song = Song()
+    private var gson : Gson = Gson()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.SplashTheme)
+        setTheme(R.style.Theme_UMC_6th)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initBottomNavigation()
-
-        val song = Song(
-            binding.mainMiniplayerTitleTv.text.toString(),
-            binding.mainMiniplayerSingerTv.text.toString(),
-            0,
-            60,
-            false
-        )
 
         activityResultLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
@@ -49,6 +44,7 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("second", song.second)
             intent.putExtra("playTime", song.playTime)
             intent.putExtra("isPlaying", song.isPlaying)
+            intent.putExtra("music",song.music)
 //            startActivity(intent)
             activityResultLauncher.launch(intent)
         }
@@ -93,5 +89,22 @@ class MainActivity : AppCompatActivity() {
             }
             false
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        val sharedPreferences = getSharedPreferences("song", MODE_PRIVATE)
+        val jsonToSong = sharedPreferences.getString("songData", null)
+        Log.d("jsonToSong", jsonToSong.toString())
+        song = if(jsonToSong == null) { // 최초 실행 시
+            Song("라일락", "아이유(IU)", 0, 60, false, "music_lilac")
+        } else { // SongActivity에서 노래가 한번이라도 pause 된 경우
+            gson.fromJson(jsonToSong, Song::class.java)
+        }
+        setMiniPlayer(song)
+    }
+    private fun setMiniPlayer(song : Song) {
+        binding.mainMiniplayerTitleTv.text = song.title
+        binding.mainMiniplayerSingerTv.text = song.singer
+        binding.mainMiniplayerProgressSb.progress = (song.second * 100000 / song.playTime)
     }
 }
