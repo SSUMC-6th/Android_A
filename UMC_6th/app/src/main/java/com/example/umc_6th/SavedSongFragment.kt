53e98f53f68ca710005cc62d9e6e3c7665dcf958
leesumin0526 <leesumin0526@gmail.com>
2024-05-20 14:42:51 +0900

    [maro/#28] feat && fix :: 보관함 switch on/off 에러 해결 && 삭제 기능 추가

--- UMC_6th/app/src/main/java/com/example/umc_6th/SavedSongFragment.kt
+++ UMC_6th/app/src/main/java/com/example/umc_6th/SavedSongFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.umc_6th.databinding.FragmentSavedSongBinding
+import com.google.gson.Gson
 
 class SavedSongFragment : Fragment() {
 
@@ -21,10 +22,21 @@ class SavedSongFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         binding = FragmentSavedSongBinding.inflate(inflater, container, false)
 
         albumDatas.apply {
+            add(Album("Butter", "방탄소년단 (BTS)", R.drawable.img_album_exp))
+            add(Album("Lilac", "아이유 (IU)", R.drawable.img_album_exp2))
+            add(Album("Next Level", "에스파 (AESPA)", R.drawable.img_album_exp3))
+            add(Album("Boy with Luv", "방탄소년단 (BTS)", R.drawable.img_album_exp4))
+            add(Album("BBoom BBoom", "모모랜드 (MOMOLAND)", R.drawable.img_album_exp5))
+            add(Album("Weekend", "태연 (Tae Yeon)", R.drawable.img_album_exp6))
+            add(Album("Butter", "방탄소년단 (BTS)", R.drawable.img_album_exp))
+            add(Album("Lilac", "아이유 (IU)", R.drawable.img_album_exp2))
+            add(Album("Next Level", "에스파 (AESPA)", R.drawable.img_album_exp3))
+            add(Album("Boy with Luv", "방탄소년단 (BTS)", R.drawable.img_album_exp4))
+            add(Album("BBoom BBoom", "모모랜드 (MOMOLAND)", R.drawable.img_album_exp5))
+            add(Album("Weekend", "태연 (Tae Yeon)", R.drawable.img_album_exp6))
             add(Album("Butter", "방탄소년단 (BTS)", R.drawable.img_album_exp))
             add(Album("Lilac", "아이유 (IU)", R.drawable.img_album_exp2))
             add(Album("Next Level", "에스파 (AESPA)", R.drawable.img_album_exp3))
@@ -37,6 +49,27 @@ class SavedSongFragment : Fragment() {
         binding.lockerMusicAlbumRv.adapter = lockerAlbumRVAdapter
         binding.lockerMusicAlbumRv.layoutManager = LinearLayoutManager(requireActivity())
 
+        lockerAlbumRVAdapter.setItemClickListener(object : LockerAlbumRVAdapter.OnItemClickListener {
+            override fun onItemClick(album: Album) {
+                changedAlbumFragment(album)
+            }
+
+            override fun onRemoveAlbum(position : Int){
+                lockerAlbumRVAdapter.removeItem(position)
+            }
+        })
         return binding.root
+    }
+
+    private fun changedAlbumFragment(album : Album) {
+        (context as MainActivity).supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, AlbumFragment().apply {
+                arguments = Bundle().apply {
+                    val gson = Gson()
+                    val albumToJson = gson.toJson(album)
+                    putString("album", albumToJson)
+                }
+            })
+            .commitAllowingStateLoss()
     }
 }
