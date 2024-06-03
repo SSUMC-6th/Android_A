601d2a765a800369c62377866bc630b35dd64b33
hyowon0204 <ally0204@naver.com>
2024-06-03 12:25:40 +0900

    [kara/#38] 8주차 구현 완료

--- UMC_6th/app/src/main/java/com/example/myfirstapp/AlbumDao.kt
+++ UMC_6th/app/src/main/java/com/example/myfirstapp/AlbumDao.kt
@@ -6,6 +6,8 @@ import androidx.room.*
 interface AlbumDao {
     @Insert
     fun insert(album: Album)
+    @Insert
+    fun likeAlbum(like : Like)
 
     @Update
     fun update(album: Album)
@@ -18,4 +20,15 @@ interface AlbumDao {
 
     @Query("SELECT * FROM AlbumTable WHERE id = :id")
     fun getAlbum(id: Int): Album
+
+    @Query("select id from LikeTable where userId =:userId and albumId = :albumId")
+    fun isLikedAlbum(userId : Int, albumId : Int) : Int?
+
+    @Query("delete from LikeTable where userId =:userId and albumId = :albumId")
+    fun dislikedAlbum(userId : Int, albumId : Int)
+
+    @Query("select at.* from LikeTable as lt left join AlbumTable as at on lt.albumId = at.id where lt.userId = :userId")
+    fun getLikedAlbums(userId : Int) : List<Album>
+    @Update
+    fun updateAlbum(album: Album)
 }
