0bbe0856a623a598f9ed078dea633bd7eef8173b
MiN <81948815+leesumin0526@users.noreply.github.com>
2024-06-05 20:30:20 +0900

    [Maro/#38] Feat :: 8주차 구현 완료 (#41)
    * [maro/#38] feat :: 로그인, 회원가입 페이지 & 회원가입 구현
    
    * [maro/#38] feat :: 좋아요 표시 유저 구분
    
    * [maro/#38] feat :: 저장 앨범 탭, 둘러보기 페이지 제작
    
    * [maro/#38] feat :: 8주차 구현 완료

--- UMC_6th/app/src/main/java/com/example/umc_6th/LockerFragment.kt
+++ UMC_6th/app/src/main/java/com/example/umc_6th/LockerFragment.kt
@@ -1,17 +1,19 @@
 package com.example.umc_6th
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.example.umc_6th.databinding.FragmentLockerBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
 class LockerFragment : Fragment() {
 
     lateinit var binding: FragmentLockerBinding
-    private var information = arrayListOf("저장한곡", "음악파일", "저장앨범")
+    private val information = arrayListOf("저장한곡", "음악파일", "저장앨범")
     val bottomSheetFragment = BottomSheetFragment()
 
 
@@ -37,6 +39,45 @@ class LockerFragment : Fragment() {
             bottomSheetFragment.show(requireFragmentManager(),"BottomSheetDialog")
         }
 
+        binding.lockerLoginTv.setOnClickListener {
+            val intent = Intent(requireActivity(), LoginActivity::class.java)
+            startActivity(intent)
+        }
+
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initViews()
+    }
+    private fun getJwt() : Int {
+        val spf = requireActivity().getSharedPreferences("auth", AppCompatActivity.MODE_PRIVATE)
+        return spf!!.getInt("jwt", 0)
+    }
+
+    private fun initViews() {
+        val jwt : Int = getJwt()
+        if (jwt == 0) {
+            binding.lockerLoginTv.text="로그인"
+            binding.lockerLoginTv.setOnClickListener {
+                startActivity(Intent(requireActivity(), LoginActivity::class.java))
+            }
+        }
+
+        else {
+            binding.lockerLoginTv.text = "로그아웃"
+            binding.lockerLoginTv.setOnClickListener {
+                logout()
+                startActivity(Intent(requireActivity(), MainActivity::class.java))
+            }
+        }
+    }
+
+    private fun logout() {
+        val spf = activity?.getSharedPreferences("auth", AppCompatActivity.MODE_PRIVATE)
+        val editor = spf!!.edit()
+        editor.remove("jwt")
+        editor.apply()
     }
 }
---
+++ UMC_6th/app/src/main/java/com/example/umc_6th/UserDao.kt
@@ -0,0 +1,17 @@
+package com.example.umc_6th
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+    @Insert
+    fun insert(user : User)
+
+    @Query("select * from UserTable")
+    fun getUsers() : List<User>
+
+    @Query("select * from UserTable where email =:email and password = :password")
+    fun getUser(email : String, password : String) : User?
+}
